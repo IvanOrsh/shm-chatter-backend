ef98474a1ace0b6288e429e72a830c16466a483d
IvanOrsh <ksironov@mail.ru>
2024-01-24 21:04:45 +0300

    feat: implement db module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
+
+import { DatabaseModule } from './common/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        MONGODB_URI: joi.string().required(),
+        // JWT_SECRET: z.string(),
+      }),
+    }),
+
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

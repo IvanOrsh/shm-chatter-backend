decc2612b785ec44408c6c75d309f966d6fdd482
IvanOrsh <ksironov@mail.ru>
2024-01-24 23:23:46 +0300

    feat: implement users repository

---
+++ src/users/users.repository.ts
@@ -0,0 +1,18 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { AbstractRepository } from 'src/common/database/abstract.repository';
+import { UserDocument } from './entities/user.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersRepository extends AbstractRepository<UserDocument> {
+  protected readonly logger = new Logger(UsersRepository.name);
+
+  constructor(
+    @InjectModel(UserDocument.name)
+    userModel: Model<UserDocument>,
+  ) {
+    super(userModel);
+  }
+}

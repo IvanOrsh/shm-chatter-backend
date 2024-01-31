1e913a339e8d18d37495b2c0edbeb5e4b92fcc43
IvanOrsh <ksironov@mail.ru>
2024-02-01 01:51:30 +0300

    feat: get currently logged user

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -44,4 +44,10 @@ export class UsersResolver {
   removeUser(@CurrentUser() user: TokenPayload) {
     return this.usersService.remove(user._id);
   }
+
+  @Query(() => User, { name: 'me' })
+  @UseGuards(GqlAuthGuard)
+  getMe(@CurrentUser() user: TokenPayload) {
+    return user;
+  }
 }

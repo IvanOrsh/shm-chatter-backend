f033d2785f29a8f7fe214592f4b5ea45ca24e404
IvanOrsh <ksironov@mail.ru>
2024-02-10 22:21:16 +0300

    feat: stub out messages

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -4,10 +4,12 @@ import { ChatsResolver } from './chats.resolver';
 import { ChatsRepository } from './chats.repository';
 import { DatabaseModule } from 'src/common/database/database.module';
 import { Chat, ChatSchema } from './entities/chat.entity';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
     DatabaseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    MessagesModule,
   ],
   providers: [ChatsResolver, ChatsService, ChatsRepository],
 })

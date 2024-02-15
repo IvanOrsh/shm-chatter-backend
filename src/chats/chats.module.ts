d6560eb459e3c51f7adeaee72c94686c16d03d42
IvanOrsh <ksironov@mail.ru>
2024-02-16 01:47:40 +0300

    fix: chat document instead of chat

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -4,13 +4,15 @@ import { ChatsResolver } from './chats.resolver';
 import { ChatsRepository } from './chats.repository';
 import { ChatsService } from './chats.service';
 import { DatabaseModule } from 'src/common/database/database.module';
-import { Chat } from './entities/chat.entity';
+import { ChatDocument } from './entities/chat.document';
 import { ChatSchema } from './entities/chat.document';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
-    DatabaseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    DatabaseModule.forFeature([
+      { name: ChatDocument.name, schema: ChatSchema },
+    ]),
     forwardRef(() => MessagesModule),
   ],
   providers: [ChatsResolver, ChatsService, ChatsRepository],
